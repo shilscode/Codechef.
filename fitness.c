10783fc70082b0f441d2de7cc97d44649d30e120
shilscode <shils787@gmail.com>
2022-06-24 23:04:00 +0530

    first

---
+++ fitness.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(void) {
+	int x,t;
+	scanf("%d",&t);
+	
+	
+	for(int  i = 0; i<t;i++){
+	   scanf("%d",&x); 
+	   int c =x*2;
+	   printf("%d\n",c*5);
+	    
+	    
+	    
+	    
+	}
+	return 0;
+}
+
