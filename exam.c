b1ea8a284104f88a4a555e0e2b35727d744fea30
shilscode <shils787@gmail.com>
2022-07-02 21:20:03 +0530

    first

---
+++ exam.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main() {
+    int  x,y,n;
+    
+    scanf("%d",&n);
+    
+    
+    
+	for (int i =0;i<n;i++){
+	        scanf("%d%d",&x,&y);
+	    
+	    if ( x >y){
+	        printf("%d\n",x);
+	    }
+	    else{
+	        printf("%d\n",y);
+	    }
+	    
+	    
+	    
+	    
+	}
+	return 0;
+}
+
